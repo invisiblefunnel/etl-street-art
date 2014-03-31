86a2a8548d4194b84638c324fea20af224716e9c
Danny Whalen <daniel.r.whalen@gmail.com>
2014-03-31 14:32:43 -0700

    Show view

--- app.rb
+++ app.rb
@@ -6,3 +6,8 @@ get '/' do
   @reports = DB[:reports].paginate(params[:page] || 1, 50)
   erb :index
 end
+
+get '/:case_id' do
+  @report = DB[:reports].where(case_id: params[:case_id]).first
+  erb :show
+end
